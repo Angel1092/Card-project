1730585eaecc615ea2831d098796a365e2e8a144
Angel1092 <angel.guerra@malad.us>
2025-12-04 16:17:44 -0800

    Update Card.java

--- Card.java
+++ Card.java
@@ -1,1 +1,16 @@
+public class Card {
+
+    private String rank;
+    private String suit;
+
+    public Card(String rank, String suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+
+    @Override
+    public String toString() {
+        return rank + " of " + suit;
+    }
+}
 
