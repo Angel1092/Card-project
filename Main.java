bd180888fe80c9466e5775284059aaf070566954
Angel1092 <angel.guerra@malad.us>
2025-12-04 16:18:09 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,1 +1,12 @@
+import java.util.ArrayList;
+import java.util.Collections;
 
+public class Main {
+
+    public static void main(String[] args) {
+
+        ArrayList<Card> deck = createDeck();
+
+        Collections.shuffle(deck);
+
+        ArrayList<ArrayList<Card>> hands = dealHands(deck, 5, 5);
