5294bf7c02b1f8b6a990a6a0a2da0a4ca34cafbc
Angel1092 <angel.guerra@malad.us>
2025-12-15 14:00:25 -0800

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,12 +2,12 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        Deck deck = new Deck();       // create a deck
-        deck.shuffle();               // shuffle it
+        Deck deck = new Deck();       // this will create a deck
+        deck.shuffle();               // this will shuffle it
 
         ArrayList<ArrayList<Card>> hands = deck.dealHands(5, 5); // deal 5 hands, 5 cards each
 
-        // Print each hand
+        // Will print each hand
         for (int i = 0; i < hands.size(); i++) {
             System.out.println("Player " + (i + 1) + ": " + hands.get(i));
         }
